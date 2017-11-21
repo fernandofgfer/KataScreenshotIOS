6e33624db9684892c07d3d07beb8cf779b8b0d14
fgarcia <fernando.garcia@tecnilogica.com>
2017-11-21 18:30:47 +0100

    some test on detail

--- KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
+++ KataScreenshotTests/SuperHeroDetailViewControllerTests.swift
@@ -13,12 +13,13 @@ import UIKit
 class SuperHeroDetailViewControllerTests: ScreenshotTest{
     
     fileprivate let repository = MockSuperHeroesRepository()
-    let superHeroName = "Mr. Clean"
+    let superHeroNameTest = "Mr. Clean"
+    let superHeroBigNameTest = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
     func testSuperHeroDetailAppearWithoutBadge(){
         givenASuperHero(isAvenger: false)
         
-        let viewController = getSuperHeroDetailViewController()
+        let viewController = getSuperHeroDetailViewController(superHeroName: superHeroNameTest)
         
         verify(viewController: viewController)
     }
@@ -26,11 +27,24 @@ class SuperHeroDetailViewControllerTests: ScreenshotTest{
     func testSuperHeroDetailAppearWithBadge(){
         givenASuperHero(isAvenger: true)
         
-        let viewController = getSuperHeroDetailViewController()
+        let viewController = getSuperHeroDetailViewController(superHeroName: superHeroNameTest)
         
         verify(viewController: viewController)
     }
     
+    func testSuperHeroWithBigName(){
+        givenASuperHeroWithBigName(isAvenger: true)
+     
+        let viewController = getSuperHeroDetailViewController(superHeroName: superHeroBigNameTest)
+        
+        verify(viewController: viewController)
+    }
+    
+    func givenASuperHeroWithBigName(isAvenger: Bool){
+        var superHeroes = [SuperHero]()
+        superHeroes.append(SuperHeroMother.givenASuperHeroWithBigName(isAvenger: isAvenger))
+        repository.superHeroes = superHeroes
+    }
     
     func givenASuperHero(isAvenger: Bool){
         var superHeroes = [SuperHero]()
@@ -38,7 +52,7 @@ class SuperHeroDetailViewControllerTests: ScreenshotTest{
         repository.superHeroes = superHeroes
     }
     
-    fileprivate func getSuperHeroDetailViewController() -> UIViewController {
+    fileprivate func getSuperHeroDetailViewController(superHeroName: String) -> UIViewController {
         let superHeroDetailViewController = ServiceLocator()
             .provideSuperHeroDetailViewController(superHeroName) as! SuperHeroDetailViewController
         superHeroDetailViewController.presenter = SuperHeroDetailPresenter(ui: superHeroDetailViewController, superHeroName: superHeroName, getSuperHeroByName: GetSuperHeroByName(repository: repository))
