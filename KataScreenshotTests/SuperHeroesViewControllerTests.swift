0d445f58d725e8f7c3f2daf0ad20de2e177cd0cb
fgarcia <fernando.garcia@tecnilogica.com>
2017-11-21 15:52:12 +0100

    fix name of method to create SuperHeroesViewController

--- KataScreenshotTests/SuperHeroesViewControllerTests.swift
+++ KataScreenshotTests/SuperHeroesViewControllerTests.swift
@@ -12,15 +12,16 @@ import UIKit
 class SuperHeroesViewControllerTests: ScreenshotTest {
 
     fileprivate let repository = MockSuperHeroesRepository()
-
+    let numberOfSuperHeroes = 10
+    
     func testShowsEmptyCase() {
         givenThereAreNoSuperHeroes()
 
-        let viewController = getSuperHeroDetailViewController()
+        let viewController = getSuperHeroViewController()
 
         verify(viewController: viewController)
     }
-
+    
     fileprivate func givenThereAreSomeAvengers() -> [SuperHero] {
         return givenThereAreSomeSuperHeroes(avengers: true)
     }
@@ -42,7 +43,7 @@ class SuperHeroesViewControllerTests: ScreenshotTest {
         return superHeroes
     }
 
-    fileprivate func getSuperHeroDetailViewController() -> UIViewController {
+    fileprivate func getSuperHeroViewController() -> UIViewController {
         let superHeroesViewController = ServiceLocator()
             .provideSuperHeroesViewController() as! SuperHeroesViewController
         superHeroesViewController.presenter = SuperHeroesPresenter(
